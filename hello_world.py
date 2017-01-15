eec43bb1a4cee8a0702b648b6e2f48c603c3f433
Merc Mercer <john.merc.mercer@gmail.com>
2017-01-15 15:45:30 -0600

    Add hello_world.py via upload

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+#! python3
+# hello_world.py - Opens the console and prints "Hello, world!".
+
+def hello():
+    return "Hello, world!"
+
+def main():
+    print(hello())
+
+if __name__ == '__main__':
+    main()
